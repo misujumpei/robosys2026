139c5fe86966e6aeb4199f0da763c5853edbf1aa
misujumpei <misujumpei@gmail.com>
2026-01-25 05:15:10 +0900

    test.bashのスペースと構文エラーを修正し動作を確認

--- test.bash
+++ test.bash
@@ -1,17 +1,13 @@
-#!/usr/bin/python3
+#!/bin/bash
 # SPDX-FileCopyrightText: 2026 misujumpei
 # SPDX-License-Identifier: BSD-3-Clause
 
 # Test 1: 正解が出るか確認 (sin 90 = 1.0)
+res=$(./trig sin 90)
 
-res=$(./trig sin 90) 	 # 実行して、結果をresに入れる
-
-
-if [ "$res" = "1.0" ]; then 	 # 「もしresの中身が 1.0 だったら」という条件分岐
-    
-    echo" 正解 "	         #正解であれば表示
+if [ "$res" = "1.0" ]; then
+    echo "正解"
 else
-    				 
-    echo "不正解: $res"　　　　　#不正解なことと、何を間違えたか表示
+    echo "不正解: $res"
     exit 1
 fi
